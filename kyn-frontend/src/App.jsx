fd0d1de88d75e3b4f0ab1e0a523076d84ca139d1
Jaiadhitya K <jaiadhi2004@gmail.com>
2024-12-31 14:56:42 +0530

    fix: tailwind initialisation

--- kyn-frontend/src/App.jsx
+++ kyn-frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Headers from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   
 
   return (
     <>
-      <h1>hello world</h1>
+      <Headers />
+      <Footer />
     </>
   )
 }
