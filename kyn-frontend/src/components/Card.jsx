6b9a0d3bef9b580fe7bce23f03cb2a9472658669
Dev <devesh2004s@gmail.com>
2024-12-31 16:10:42 +0530

    feat: bgchange

--- kyn-frontend/src/components/Card.jsx
+++ kyn-frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Card = ({ title, value, percentageChange }) => {
   return (
-    <div className="bg-purple-100 p-4 rounded-lg text-center">
+    <div className="bg-purple-300 p-4 rounded-lg text-center">
       <p className="text-sm font-medium">{title}</p>
       <h3 className="text-2xl font-bold">{value}</h3>
       {percentageChange && (
