6637f5efcb55f71081ba0d9103448e44042e8c71
akeelahamed24 <akeelahamed24.2k5@gmail.com>
2024-12-31 15:22:02 +0530

    feat: Button2

--- kyn-frontend/src/App.jsx
+++ kyn-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Headers from './components/Header'
 import Footer from './components/Footer'
 import  Button  from './components/Button'
 import Dashboard from './screens/Dashboard'
+import Button2 from './components/Button2'
 
 function App() {
   
@@ -13,6 +14,9 @@ function App() {
       <Headers />
       <div className="flex justify-center mt-8">
         <Button onClick={() => alert("Button Clicked!")}>Get Started</Button>
+      </div>
+      <div className="flex justify-center mt-8">
+        <Button2 onClick={() => alert("Button Clicked!")}>Get Started</Button2>
       </div>
       <Dashboard />
       <Footer />
