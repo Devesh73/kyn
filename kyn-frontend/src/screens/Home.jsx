6ccb6b33985ce080a05438c6d125a2391c5f2031
Jaiadhitya-K <jaiadhi2004@gmail.com>
2025-03-28 08:37:31 +0530

    fix: logo and elements

--- kyn-frontend/src/screens/Home.jsx
+++ kyn-frontend/src/screens/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <section className="flex justify-between items-center py-16 px-8 bg-slate-950 shadow-lg h-screen min-w-[300px] max-w-[100vw] flex-wrap">
       <div className="text-white max-w-[50%] pr-8">
-        <h1 className="text-4xl font-bold mb-4  text-purple-700">Welcome to the KYN Dashboard</h1>
-        <p className="text-xl mb-6  text-purple-700">Empowering employees with AI-driven solutions</p>
+        <h1 className="text-4xl font-bold mb-4  text-purple-700">Welcome to the Project SARVAM</h1>
+        <p className="text-xl mb-6  text-purple-700">Empowering AI-driven solutions to Information</p>
         <NavLink
           to="/login"
           className="inline-block text-lg font-semibold text-purple-700 border-2 border-purple-700 px-6 py-3 rounded-lg transition duration-300 ease-in-out hover:bg-purple-700 hover:text-white"
